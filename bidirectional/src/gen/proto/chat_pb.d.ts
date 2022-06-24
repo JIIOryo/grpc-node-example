ff0479d15b14c58ee08c3e72668ef25415097276
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2022-06-24 21:41:25 +0900

    Add stamp

--- bidirectional/src/gen/proto/chat_pb.d.ts
+++ bidirectional/src/gen/proto/chat_pb.d.ts
@@ -27,3 +27,27 @@ export namespace ChatMessage {
   }
 }
 
+export class StampMessage extends jspb.Message {
+  getFrom(): string;
+  setFrom(value: string): void;
+
+  getStampId(): string;
+  setStampId(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): StampMessage.AsObject;
+  static toObject(includeInstance: boolean, msg: StampMessage): StampMessage.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: StampMessage, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): StampMessage;
+  static deserializeBinaryFromReader(message: StampMessage, reader: jspb.BinaryReader): StampMessage;
+}
+
+export namespace StampMessage {
+  export type AsObject = {
+    from: string,
+    stampId: string,
+  }
+}
+
